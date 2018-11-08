b2dfb34a8793ff4d9cae098c227138f38ff78bb6
Dezmon Gilbert <dog6487@rit.edu>
2018-11-08 12:17:05 -0500

    Fixed circle ci

--- server/controllers/Post.js
+++ server/controllers/Post.js
@@ -2,65 +2,65 @@ const models = require('../models');
 
 const Post = models.Post;
 
-const dashPage = (req, res) =>{
-    res.render('app', { csrfToken: req.csrfToken() });
+const dashPage = (req, res) => {
+  res.render('app', { csrfToken: req.csrfToken() });
 };
 
 const addPost = (req, res) => {
-    if(!req.body.title || !req.body.text){
-        return res.status(400).json({ error: 'All fields are required' });
-    }; 
+  if (!req.body.title || !req.body.text) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
 
-    const postData = {
-        title: req.body.title,
-        text: req.body.text,
-        tags: [],
-        owner: req.session.account._id
-    };
+  const postData = {
+    title: req.body.title,
+    text: req.body.text,
+    tags: [],
+    owner: req.session.account._id,
+  };
 
-    const newPost = new Post.PostModel(postData);
+  const newPost = new Post.PostModel(postData);
 
-    const postPromise = newPost.save();
+  const postPromise = newPost.save();
 
-    postPromise.then(() => res.json({ redirect: '/dashboard' }));
+  postPromise.then(() => res.json({ redirect: '/dashboard' }));
 
-    postPromise.catch((err) => {
-        console.log(err);
-        if (err.code === 11000) {
-            return res.status(400).json({ error: 'Post already exists' });
-        }
-        return res.status(400).json({ error: 'An error occured' });
+  postPromise.catch((err) => {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Post already exists' });
+    }
+    return res.status(400).json({ error: 'An error occured' });
   });
 
   return postPromise;
 };
 
-const getPosts = (request, response) =>{
-    const req = request;
-    const res = response;
-  
-    return Post.PostModel.findMostRecent((err, docs) => {
-      if (err) {
-        console.log(err);
-        return res.status(400).json({ error: 'An error occured' });
-      }
-  
-      return res.json({ posts: docs, userID: req.session.account._id });
-    });
+const getPosts = (request, response) => {
+  const req = request;
+  const res = response;
+
+  return Post.PostModel.findMostRecent((err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occured' });
+    }
+
+    return res.json({ posts: docs, userID: req.session.account._id });
+  });
 };
 
 const deletePost = (request, response) => {
-    const req = request;
-    const res = response;
-  
-    return Post.PostModel.removeByID(req.body._id, (err) => {
-      if (err) {
-        console.log(err);
-        return res.status(400).json({ error: 'An error occured' });
-      }
-      return res.status(204).json();
-    });
-  };
+  const req = request;
+  const res = response;
+
+  return Post.PostModel.removeByID(req.body._id, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occured' });
+    }
+    return res.status(204).json();
+  });
+};
 
 module.exports.dashPage = dashPage;
 module.exports.getPosts = getPosts;
--- server/models/Post.js
+++ server/models/Post.js
@@ -5,56 +5,54 @@ mongoose.Promise = global.Promise;
 let PostModel = {};
 
 const PostSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true,
-    },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-    text: {
-        type: String,
-        required: true,
-        trim: true,
-    },
+  text: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-    tags: {
-        type: mongoose.Schema.Types.Array,
-        required: false,
-        default: [],
-    },
+  tags: {
+    type: mongoose.Schema.Types.Array,
+    required: false,
+    default: [],
+  },
 
-    owner: {
-        type: mongoose.Schema.ObjectId,
-        required: true,
-        ref: 'Account',
-    },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
 
-    createdDate: {
-        type: Date,
-        default: Date.now,
-    },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 PostSchema.statics.toAPI = doc => ({
-    title: doc.title,
-    text: doc.text,
-    tags: doc.tags,
-    owner: doc.owner,
-    createdDate: doc.createdDate,
-    _id: doc._id,
+  title: doc.title,
+  text: doc.text,
+  tags: doc.tags,
+  owner: doc.owner,
+  createdDate: doc.createdDate,
+  _id: doc._id,
 });
 
-PostSchema.statics.findMostRecent = callback => {
-    return PostModel.find().sort({createdDate: -1}).exec(callback);
-};
+PostSchema.statics.findMostRecent = callback => PostModel.find().sort({ createdDate: -1 }).exec(callback);
 
 PostSchema.statics.removeByID = (docID, callback) => {
-    const search = {
-      _id: docID,
-    };
-  
-    return PostModel.find(search).remove().exec(callback);
+  const search = {
+    _id: docID,
   };
+
+  return PostModel.find(search).remove().exec(callback);
+};
 
 PostModel = mongoose.model('Post', PostSchema);
 
